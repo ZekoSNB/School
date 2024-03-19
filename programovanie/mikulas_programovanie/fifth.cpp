d97362d187559ff7d88ea304cb0d9d3024b346de
Maros Tomasov <maros3845@gmail.com>
2024-03-19 07:45:22 +0100

    adding files

---
+++ programovanie/mikulas_programovanie/fifth.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char c;
+    long num;
+    long sum = 0;
+    while ((cin.get()) != '\n'){
+        
+    }
+    // cout << sum << endl;
+    return 0;
+}
---
+++ programovanie/mikulas_programovanie/fourth.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char charakter[1000]; 
+    long poc = 0;
+    long ha = 0;
+    
+    for (int i = 0; i < sizeof(charakter); i++){
+        for (int j = 0; j < i; j++){
+            
+        }
+    }
+    cout << poc << endl;
+    cout << ha << endl;
+    return 0;
+}
